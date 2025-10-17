c7603ef437e27a8ec6b553df179b7b052d2ba318
Micheal Nestor <michealnestor@outlook.com>
2025-10-17 08:23:35 +0100

    added second test and made it pass

--- src/rover.py
+++ src/rover.py
@@ -1,2 +1,4 @@
 def execute(command: str) -> str:
+    if command == "M":
+        return "0:1:N"
     return "0:0:N"
--- tests/rover_test.py
+++ tests/rover_test.py
@@ -4,3 +4,6 @@ from src.rover import execute
 class TestExecute(unittest.TestCase):
     def test_no_command(self):
         self.assertTrue(execute("") == "0:0:N")
+
+    def test_move_once(self):
+        self.assertTrue(execute("M") == "0:1:N")
