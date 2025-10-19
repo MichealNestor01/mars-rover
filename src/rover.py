ad72fc1ed93b32d5619861c5dba571382e23b32f
Micheal Nestor <michealnestor@outlook.com>
2025-10-19 22:38:52 +0100

    cleaned up the code

--- src/rover.py
+++ src/rover.py
@@ -18,6 +18,29 @@ DEFAULT_ORIENTATION_MAP = { # angle from the y-axis in radians
     np.round(-np.pi/2, DEFAULT_ROUND_TO): "W",
 }
 
+def _rotate_clockwise(vector: np.ndarray[float], theta: float) -> np.ndarray[float]:
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    rotation_matrix = np.array((
+        (cos_theta, sin_theta),
+        (-sin_theta, cos_theta)
+    ))
+    return rotation_matrix @ vector
+
+def _update_location(
+        loc: np.ndarray[float], 
+        rot: np.ndarray[float],
+        obstacles: list[np.ndarray[float]],
+        movement_scalar: float
+    ) -> list[np.ndarray, bool]: # new_loc, hit_obstacle 
+    if len(obstacles):
+        new_loc = loc + (movement_scalar * rot)
+        for o in obstacles: 
+            if np.allclose(o, new_loc):
+                return loc, True
+        return new_loc, False
+    return loc + (movement_scalar * rot), False
+
 def execute(
         command: str,
         grid_width: int = DEFAULT_GRID_WIDTH,
@@ -25,39 +48,27 @@ def execute(
         movement_map: dict[str, float] = DEFAULT_MOVEMENT_MAP,
         rotation_map: dict[str, float] = DEFAULT_ROTATION_MAP,
         orientation_map: dict[float, str] = DEFAULT_ORIENTATION_MAP,
-        obstacles: list[list[float]] = [],
+        obstacles: list[np.ndarray[float]] = [],
         round_to: int = DEFAULT_ROUND_TO,
     ) -> str:
     '''mars rover kata solution'''
-    loc = np.array((0.0, 0.0)) # location vector, (x, y)
-    rot = np.array((0.0, 1.0)) # direction vector, starting looking north to (0, 1)
+
+    loc, rot = np.array((0.0, 0.0)), np.array((0.0, 1.0)) # start (0,0) looking north
     hit_obstacle = False
+
+    # move the robot
     for char in command:
         if char in movement_map:
-            if len(obstacles):
-                new_loc = loc + (movement_map.get(char) * rot)
-                for o in obstacles: 
-                    if np.allclose(o, new_loc):
-                        hit_obstacle = True
-                        break
-                if hit_obstacle:    
-                    break
-                loc = new_loc
-            else:
-                loc += movement_map.get(char) * rot
+            loc, hit_obstacle = _update_location(loc, rot, obstacles, movement_map.get(char))
+            if hit_obstacle:
+                break
         elif char in rotation_map:
-            theta = rotation_map.get(char)
-            sin_theta = np.sin(theta)
-            cos_theta = np.cos(theta)
-            rotation_matrix = np.array((
-                (cos_theta, sin_theta),
-                (-sin_theta, cos_theta)
-            ))
-            rot = rotation_matrix @ rot
+            rot = _rotate_clockwise(rot, rotation_map.get(char))
         #  ensure grid isn't exceeded
         loc[0] %= grid_width
         loc[1] %= grid_height
-    # to convert the rotation vector into an angle from the y axis
+    
+    # convert the rotation matrix to angle from (0, 1) and format the output string
     rot_angle = np.round(np.arctan2(*rot), round_to)
     orientation = orientation_map.get(rot_angle, "N")
     x_val, y_val = np.round(loc).astype(int)
